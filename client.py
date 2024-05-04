937354e9716bfb5a35952467004d889502e6c8c5
Shaked <shakeduzan77@gmail.com>
2024-05-04 18:00:11 +0300

    scanFile

--- client.py
+++ client.py
@@ -1,4 +1,11 @@
 import requests
-file_path= input.next("enter the filepath to the filr you want to check")
+file_path= input.next("enter the filepath to the file you want to check")
 APIkey= '631396087b7bdcd13074d93bccf5a69c8b5462ee5cc9018ddc4e1354e0df8137'
-url= 'https://www.virustotal.com/vtapi/v2/url/scan'
+
+def scanFile(APIkey, file_path):
+    url= 'https://www.virustotal.com/vtapi/v2/url/scan'
+    request = requests.post(url, file_path, APIkey)
+    result = request.json()
+
+    if 'scan_id' in result:
+        return result['scan_id']
