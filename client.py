452e33071207846febb4b06f306acef4aed40f2a
Shaked <shakeduzan77@gmail.com>
2024-05-04 18:07:15 +0300

    def_Implement

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import requests
-file_path= input.next("enter the filepath to the file you want to check")
+file_path= input.next("enter the path to the file you want to scan")
 APIkey= '631396087b7bdcd13074d93bccf5a69c8b5462ee5cc9018ddc4e1354e0df8137'
 
 def scanFile(APIkey, file_path):
@@ -9,3 +9,6 @@ def scanFile(APIkey, file_path):
 
     if 'scan_id' in result:
         return result['scan_id']
+    
+scan_id = scanFile(APIkey, file_path)
+print("File submitted for scanning. Scan ID:", scan_id)
