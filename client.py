1d889be455db6cd26671082f92f594b35eacc9ce
Shaked <shakeduzan77@gmail.com>
2024-05-04 17:15:46 +0300

    dataUpdate

---
+++ client.py
@@ -0,0 +1,4 @@
+import requests
+file_path= input.next("enter the filepath to the filr you want to check")
+APIkey= '631396087b7bdcd13074d93bccf5a69c8b5462ee5cc9018ddc4e1354e0df8137'
+url= 'https://www.virustotal.com/vtapi/v2/url/scan'
