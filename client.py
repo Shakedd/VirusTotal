f69cb5ea41e85ca7300f4513c075ee25213fe692
Shaked <shakeduzan77@gmail.com>
2024-05-04 18:19:59 +0300

    corrections

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import requests
-file_path= input.next("enter the path to the file you want to scan")
+file_path= input("enter the path to the file you want to scan")
 APIkey= '631396087b7bdcd13074d93bccf5a69c8b5462ee5cc9018ddc4e1354e0df8137'
 
 def scanFile(APIkey, file_path):
     url= 'https://www.virustotal.com/vtapi/v2/url/scan'
-    request = requests.post(url, file_path, APIkey)
+    params = {'apikey': APIkey}
+    files = {'file': open(file_path, 'rb')}
+    request = requests.post(url, files=files, params=params)
     result = request.json()
 
     if 'scan_id' in result:
