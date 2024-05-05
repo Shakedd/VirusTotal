9aece9ef54c4091615e92e2580682c9a33bb1e09
Shaked <shakeduzan77@gmail.com>
2024-05-05 20:27:04 +0300

    variable print in order to investigate what the problem is with the code

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import requests
-file_path= input("enter the path to the file you want to scan")
+file_path= input("enter the path to the file you want to scan: ")
 APIkey= '631396087b7bdcd13074d93bccf5a69c8b5462ee5cc9018ddc4e1354e0df8137'
 
 def scanFile(APIkey, file_path):
@@ -8,10 +8,12 @@ def scanFile(APIkey, file_path):
     files = {'file': open(file_path, 'rb')}
     request = requests.post(url, files=files, params=params)
     json_result = request.json()
+    print("Response from VirusTotal API:", json_result)  # Print response for debugging
 
     ## in case json_result is a list:
     if isinstance(json_result, list):
         for result in json_result:
+            print("result: ", result)  # Print result for debugging
             if result.get('response_code') == 1:
                 if result.get('positives', 0) == 0:
                     return "your file is secure"
