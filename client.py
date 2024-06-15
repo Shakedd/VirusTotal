947beb1711900b09bfd380d587d48d2a130ab377
Shaked <shakeduzan77@gmail.com>
2024-06-15 16:41:12 +0300

    url fix

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ file_path= input("enter the path to the file you want to scan: ")
 APIkey= '631396087b7bdcd13074d93bccf5a69c8b5462ee5cc9018ddc4e1354e0df8137'
 
 def scanFile(APIkey, file_path):
-    url= 'https://www.virustotal.com/vtapi/v2/url/scan'
+    url= 'https://www.virustotal.com/vtapi/v2/file/scan'
     params = {'apikey': APIkey}
     files = {'file': open(file_path, 'rb')}
     request = requests.post(url, files=files, params=params)
