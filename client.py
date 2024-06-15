d108401845d50da0883f0ffaacc19afd98229d78
Shaked <shakeduzan77@gmail.com>
2024-06-15 16:55:40 +0300

    delete list option

--- client.py
+++ client.py
@@ -7,30 +7,7 @@ def scanFile(APIkey, file_path):
     params = {'apikey': APIkey}
     files = {'file': open(file_path, 'rb')}
     request = requests.post(url, data=params, files=files)
-    json_result = request.json()
-    print("Response from VirusTotal API:", json_result)  # Print response for debugging
 
-    ## in case json_result is a list:
-    if isinstance(json_result, list):
-        for result in json_result:
-            print("result: ", result)  # Print result for debugging
-            if result.get('response_code') == 1:
-                if result.get('positives', 0) == 0:
-                    return "your file is secure"
-                else:
-                    return "your file is not secure"
-        ## in case none of the conditions work:
-        return "Error, can't determine the file security status"
-    ## in case json_result is not a list:
-    elif isinstance(json_result, list) == False:
-        if json_result.get('response_code') == 1:
-                if json_result.get('positives', 0) == 0:
-                    return "your file is secure"
-                else:
-                    return "your file is not secure"
-        ## in case something went wrong:
-    else:
-        return "Eror, something went wrong"
-    
+   
 finalResult = scanFile(APIkey, file_path)
 print("File submitted for scanning. your result:", finalResult)
